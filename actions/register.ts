68532b44a8493cc43fb0bd2c52d1f3244cf97d00
Vasin Suksuchit <bestwasin1617@gmail.com>
2024-01-23 21:44:27 +0700

    feat:Create User

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { db } from "@/lib/db";
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatgedFields = RegisterSchema.safeParse(values);
@@ -11,5 +14,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { succes: "Email Sent!" };
+  const { email, password, name } = validatgedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Ensure Email has not taken
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO : Send verificaiton token email
+
+  return { succes: "User created!" };
 };
