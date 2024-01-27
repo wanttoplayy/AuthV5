c4ced1e5dc0e53dccaf91431882b7f417cf06a5d
Vasin Suksuchit <bestwasin1617@gmail.com>
2024-01-27 15:22:43 +0700

    feat:middleware and loginRoutes

--- actions/login.ts
+++ actions/login.ts
@@ -2,7 +2,10 @@
 
 import * as z from "zod";
 
+import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatgedFields = LoginSchema.safeParse(values);
@@ -11,5 +14,23 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { succes: "Email Sent!" };
+  const { email, password } = validatgedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid Credential!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
 };
---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,22 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+const SettingsPage = async () => {
+  const session = await auth();
+
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}
+      >
+        <Button typeof="submit">Sign Out</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SettingsPage;
